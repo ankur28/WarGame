f505fa74d614fbfdea4754c3cf7ca16e1e81d175
Ankur Kalson <ankurkalson@Ankurs-MacBook-Pro.local>
2022-10-09 19:22:52 -0400

    first commit

---
+++ war challenge/war challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war challenge
+//
+//  Created by Ankur Kalson on 2022-10-09.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
